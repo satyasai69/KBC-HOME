2f4ccc52ebb398726a4ae24f395db1d236414ac8
satyasai69 <ch.satya.sai133@gmail.com>
2024-05-06 17:54:44 +0530

    hi

---
+++ index.js
@@ -0,0 +1,32 @@
+// 2583 line
+<script
+  src="https://assets-global.website-files.com/64f4d4be366fe5c8cbd6904c/js/webflow.507464012.js"
+  type="text/javascript"
+></script>;
+
+/***
+ * line 1730
+ * 
+ * 
+ *  <div id="banner-membership" class="bannerwrapper membershipcta">
+          <div class="ctabannertext">
+            <img
+              src="./logo.jpeg"
+              loading="lazy"
+              alt=""
+              class="img-banner-small bigger"
+            />
+            <h2 class="heading-2">MEMBERSHIP</h2>
+            <div>
+              Our North Star has always been to connect you to your highest
+              destiny through the stars, in a way that resonates best with your
+              journey. We &#x27;re thrilled to offer these tiers, and we can
+              &#x27;t wait for you to find your perfect cosmic fit.
+            </div>
+            <a
+              href="https://dex-web.vercel.app/"
+              class="primary-button-white upgrademodalbutton w-button"
+              >SUBSCRIBE NOW</a
+            >
+          </div>
+ */
